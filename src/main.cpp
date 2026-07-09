0be3805fcd1911c079c9532f51cec1dbf8d28656
Arjun Tyagi <arjun25@illinois.edu>
2026-07-09 17:22:27 -0500

    Update main with code cleaup

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,11 @@
-#include<cache.hpp>
 #include<iostream>
 #include<vector>
 #include<stdlib.h>
 #include<fstream>
 #include<sstream>
 #include<cstdint>
+#include<cache.hpp>
+#include<memory.hpp>
 
 using namespace std;
 
@@ -26,7 +27,7 @@ void print_cache_data(cache* C)
     cout << "Valid\tTag\tData" << endl;
     for (size_t i = 0; i < cache_data.size(); i++)
     {
-        cout << cache_lines[i]->get_valid() << "\t" << cache_lines[i]->get_tag() << "\t";
+        cout << cache_lines[i]->get_valid() << "\t" << cache_lines[i]->get_tag().value() << "\t";
         print_line_data(cache_data[i]);
     }
     cout << "------------------------------------------------------------" << endl;
@@ -63,19 +64,25 @@ vector<vector<string>> read_trace_file(string trace_file_path)
 
 int main(int argc, char** argv)
 {
-    if (argc < 9)
+    if (argc < 13)
     {
-        cerr << "Missing arguments. Usage: " << argv[0] << " --cache_size <cache size in kB> --line_size <line size in kB> --assoc <associativity> --trace_file <path to trace file>" << endl;
+        cerr << "Missing arguments. Usage: " << argv[0] << " --memory_size <memory size in kB> --page_size <page size in kB> --cache_size <cache size in kB> --line_size <line size in kB> --assoc <associativity> --trace_file <path to trace file>" << endl;
         return -1;
     }
+    size_t memory_size = 8192;
+    size_t page_size = 4096;
     size_t cache_size = 64;
     size_t line_size = 8;
     size_t assoc = 4;
     string trace_file_path;
 
-    for (int i = 1; i < 8; i += 2)
+    for (int i = 1; i < 12; i += 2)
     {
-        if (!strcmp(argv[i], "--cache_size"))
+        if (!strcmp(argv[i], "--memory_size"))
+            memory_size = strtoull(argv[i+1], NULL, 10);
+        else if (!strcmp(argv[i], "--page_size"))
+            page_size = strtoull(argv[i+1], NULL, 10); 
+        else if (!strcmp(argv[i], "--cache_size"))
             cache_size = strtoull(argv[i+1], NULL, 10);
         else if (!strcmp(argv[i], "--line_size"))
             line_size = strtoull(argv[i+1], NULL, 10); 
@@ -85,23 +92,26 @@ int main(int argc, char** argv)
             trace_file_path = argv[i+1];
         else
         {
-            cerr << "Undefined flag. Usage: " << argv[0] << " --cache_size <cache size in kB> --line_size <line size in kB> --assoc <associativity>" << endl;
+            cerr << "Undefined flag. Usage: " << argv[0] << " --memory_size <memory size in kB> --page_size <page size in kB> --cache_size <cache size in kB> --line_size <line size in kB> --assoc <associativity>" << endl;
             return -1;
         }        
     }
 
-    size_t offset_bits = log2(line_size);
-    size_t offset_mask = (1u << offset_bits) - 1;
+    size_t line_offset_bits = log2(line_size);
+    size_t page_offset_bits = log2(page_size);
     size_t index_bits = log2(cache_size/(line_size*assoc));
-    size_t index_mask = (1u << index_bits) - 1;
+    int accesses = 0, hits = 0, misses = 0;
 
 
     cout << "========================SIMULATION PARAMETERS========================" << endl;
+    cout << "Memory size (kB): " << memory_size << endl;
+    cout << "Page size (kB): " << page_size << endl;
     cout << "Cache size (kB): " << cache_size << endl;
     cout << "Line size (kB): " << line_size << endl;
     cout << "Associativity: " << assoc << endl;
     cout << "Trace path: " << trace_file_path << endl;
-    cout << "Number of offset bits: " << offset_bits << endl;
+    cout << "Number of page offset bits: " << page_offset_bits << endl;
+    cout << "Number of line offset bits: " << line_offset_bits << endl;
     cout << "Number of index bits: " << index_bits << endl;
     cout << "=====================================================================" << endl;
 
@@ -116,10 +126,17 @@ int main(int argc, char** argv)
 
     vector<vector<string>> trace = read_trace_file(trace_file_path);
 
+    memory* M = new memory(memory_size, page_size, line_size, "random");
     cache* C = new cache(cache_size, line_size, assoc); 
+
+    cout << "--------------------Initial State of Memory--------------------" << endl;
+    M->print_memory_data();
+    cout << "--------------------Initial State of Cache--------------------" << endl;
+    C->print_cache_data();
     
     for (size_t lines = 0; lines < trace.size(); lines++)
     {
+        accesses++;
         string operation = trace[lines][0];
         size_t address = static_cast<size_t>(stoul(trace[lines][1], nullptr, 16));
         size_t data;
@@ -128,33 +145,60 @@ int main(int argc, char** argv)
 
         if (operation == "R")
         {
-            size_t byte = static_cast<size_t>(C->find_byte(address));
-            if (byte != NULL)
-                cout << "Found the data " << byte << " at address: " << address << endl; 
+            cout << "-----------------------------------------------------------" << endl; 
+            cout << "Trace: " << operation << " " << address << endl;
+            optional<u_int8_t> result = C->find_byte(address);
+            if (result.has_value())
+            {
+                size_t byte = static_cast<size_t>(*result);
+                cout << "Read Hit! Found the data " << byte << " at address " << address << endl; 
+                hits++;
+            }
             else
-                cout << "Line containing address " << address << " was not found in the cache." << endl;
+            {
+                cout << "Read Miss! Line containing address " << address << " was not found in the cache. Requesting from memory..." << endl;
+                line* l = M->get_line(address);
+                pair<line*, size_t> evicted_line = C->replace_line(l, address);
+                if (evicted_line.first != nullptr)
+                    M->write_line(evicted_line.first, evicted_line.second);
+                misses++;
+            }
         }
         else if (operation == "W")
         {
+            cout << "-----------------------------------------------------------" << endl; 
+            cout << "Trace: " << operation << " " << address << " " << data << endl;
             bool write_successful = C->write_byte(address, data);
             if (write_successful)
             {
-                cout << "Found the line containing address " << address << " in the cache so writing data " << data << " at that address was successful." << endl;
+                hits++;
+                cout << "Write Hit! Found the line containing address " << address << " in the cache so writing data " << data << " at that address was successful." << endl;
             }
             else
             {
-                cout << "Line containing address " << address << " is not present in the cache so need to request the line from lower-level cache" << endl;
-                // Putting a dummy line for now
-                size_t offset = (address & offset_mask);
-                size_t tag = (address >> (index_bits + offset_bits));
-                line* dummy_line = new line(line_size);
-                dummy_line->write_byte(data, tag, offset);
-                C->replace_line(dummy_line, address);
+                misses++;
+                cout << "Write Miss! Line containing address " << address << " was not found in the cache. Requesting from memory..." << endl;
+                line* requested_line = M->get_line(address);
+                pair<line*, size_t> evicted_line = C->replace_line(requested_line, address);
+                if (evicted_line.first != nullptr)
+                    M->write_line(evicted_line.first, evicted_line.second);
+
+                bool write_successful = C->write_byte(address, data);
+                if (write_successful)
+                {
+                    cout << "Found the line containing address " << address << " in the cache so writing data " << data << " at that address was successful." << endl;
+                }
             }
         }
-
-        print_cache_data(C);
+        C->print_cache_data();
+        cout << "-----------------------------------------------------------" << endl;
     }
+
+    cout << "------------------------FINAL STATS------------------------" << endl;
+    cout << "Accesses: " << accesses << endl;
+    cout << "Hits: " << hits << endl;
+    cout << "Misses: " << misses << endl;
+    cout << "-----------------------------------------------------------" << endl;
     
     return 1;
 }
