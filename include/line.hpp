402ab746175af134b51c87963c5d234ed59116c8
Arjun Tyagi <arjun25@illinois.edu>
2026-07-09 12:53:19 -0500

    Update line.hpp and line.cpp code
    This commit updates the line class. The following changes are made:
    - Added #pragma once in the header file.
    - User can define how they want the line to be initialized. Options are
      all zeros, all ones, or random.
    - Added a dirty bit to keep track of dirty lines
    - Added print_line_data that prints out the line data along with the
      metadata info like tag, valid bit, dirty bit.

--- include/line.hpp
+++ include/line.hpp
@@ -1,21 +1,26 @@
+#pragma once
+
 #include<cstdint>
 #include<vector>
 
 class line
 {
     public:
-        line(size_t line_size_);
+        line(size_t line_size_, bool valid_, std::string init_);
         u_int8_t get_byte(size_t offset_);
         std::vector<u_int8_t> get_line_data();
         void write_byte(u_int8_t write_data_, size_t tag_, size_t offset_);
         void write_line(std::vector<u_int8_t> write_data_, size_t tag_);
         size_t get_tag();
         bool get_valid();
+        bool get_dirty_bit();
+        void print_line_data();
 
     private:
         std::vector<u_int8_t> line_data;
         size_t tag;
         bool valid; 
+        bool dirty;
         size_t line_size;
 };
 
--- src/line.cpp
+++ src/line.cpp
@@ -1,13 +1,27 @@
+#include<random>
+#include<iostream>
 #include<line.hpp>
 
-line::line(size_t line_size_)
+using namespace std;
+
+line::line(size_t line_size_, bool valid_, string init_)
 {
     this->line_size = line_size_;
+
+    random_device rd;
+    mt19937 generator(rd());
+    uniform_int_distribution<uint16_t> dist(0x00, 0xFF);
     for (size_t i = 0; i < this->line_size; i++)
     {
-        this->line_data.push_back(0x00);
+        if (init_ == "zeros")
+            this->line_data.push_back(0x00);
+        else if (init_ == "ones")
+            this->line_data.push_back(0xFF);
+        else if (init_ == "random")
+            this->line_data.push_back(static_cast<uint8_t>(dist(generator)));
     }
-    this->valid = false;
+    this->valid = valid_;
+    this->dirty = false;
 }
 
 u_int8_t line::get_byte(size_t offset_)
@@ -25,6 +39,7 @@ void line::write_byte(u_int8_t write_data_, size_t tag_, size_t offset_)
     this->line_data[offset_] = write_data_;
     this->tag = tag_;
     this->valid = true;
+    this->dirty = true;
 }
 
 void line::write_line(vector<u_int8_t> write_data_, size_t tag_)
@@ -32,6 +47,7 @@ void line::write_line(vector<u_int8_t> write_data_, size_t tag_)
     this->line_data = write_data_;
     this->tag = tag_;
     this->valid = true;
+    this->dirty = true;
 }
 
 size_t line::get_tag()
@@ -42,4 +58,18 @@ size_t line::get_tag()
 bool line::get_valid()
 {
     return this->valid;
+}
+
+bool line::get_dirty_bit()
+{
+    return this->dirty;
+}
+
+void line::print_line_data()
+{
+    cout << this->valid << "\t" << this->dirty << "\t" << this->tag << "\t";
+    for (size_t i = 0; i < this->line_size; i++)
+    {
+        cout << hex << static_cast<int>(this->line_data[i]) << " ";
+    }
 }
