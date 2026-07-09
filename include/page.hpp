ae5d9538a86449f3a75864c35bcbe3f3ab20a7c6
Arjun Tyagi <arjun25@illinois.edu>
2026-07-09 12:58:25 -0500

    Update page class code
    This commit updates the page class code. The following changes are made:
    - Added #pragma once to the header file.
    - Constructor takes in an init_ argument to define how the pages are
      initialized. Options are all zeros, all ones, or random.
    - print_page_data() function that prints the contents of the page.

--- include/page.hpp
+++ include/page.hpp
@@ -1,17 +1,19 @@
+#pragma once
+
 #include<vector>
 #include<line.hpp>
 
 class page
 {
     public:
-        page(size_t page_size_, size_t line_size);
+        page(size_t page_size_, size_t line_size, std::string init_);
         line* get_line(size_t VA_);
         void write_line(line* l_, size_t VA_);
+        void print_page_data();
 
     private:
         std::vector<line*> page_lines;
         size_t page_size;
         size_t line_size;
         size_t number_of_lines;
-        vector<pair<size_t, line*>> address_lines_pairs;
 };
--- src/page.cpp
+++ src/page.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-page::page(size_t page_size_, size_t line_size_)
+page::page(size_t page_size_, size_t line_size_, string init_)
 {
     this->page_size = page_size_;
     this->line_size = line_size_;
@@ -16,7 +16,7 @@ page::page(size_t page_size_, size_t line_size_)
     
     for (size_t i = 0; i < this->number_of_lines; i++)
     {
-        line* l = new line(this->line_size);
+        line* l = new line(this->line_size, true, init_);
         this->page_lines.push_back(l);
     }
 
@@ -30,11 +30,22 @@ line* page::get_line(size_t VA_)
     return this->page_lines[line_index];
 }
 
-void page::write_line(line* l_, size_t VA_)
+void page::write_line(line* l_, size_t address_)
 {
-    size_t offset_bit = log2(this->line_size);
-    size_t line_index = VA_ >> offset_bit;
+    size_t line_offset_bit = log2(this->line_size);
+    size_t line_index = address_ >> line_offset_bit;
 
     this->page_lines[line_index] = l_;
+}
+
+void page::print_page_data()
+{
+    cout << "Line #\tValid\tDirty\tTag\tData" << endl;
+    for (size_t i = 0; i < this->number_of_lines; i++)
+    {
+        cout << i << "\t";
+        this->page_lines[i]->print_line_data();
+        cout << endl;
+    }
 }
 
