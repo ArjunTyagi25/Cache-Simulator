f1a27324b8193e8d11b13045e6802d8e50e8209f
ArjunTyagi25 <arjun25@illinois.edu>
2026-07-08 22:47:19 -0500

    Update line.hpp and cache.hpp to not use namespace

--- include/cache.hpp
+++ include/cache.hpp
@@ -8,8 +8,8 @@ class cache
         u_int8_t find_byte(size_t address_);
         bool write_byte(size_t address_, u_int8_t write_data_);
         line* replace_line(line* new_line_, size_t address_);
-        vector<line*> get_cache_lines();
-        vector<vector<u_int8_t>> get_cache_data();
+        std::vector<line*> get_cache_lines();
+        std::vector<std::vector<u_int8_t>> get_cache_data();
 
 
     private:
@@ -22,5 +22,5 @@ class cache
         size_t offset_mask;
         size_t index_bits;
         size_t index_mask;
-        vector<line*> cache_lines;
+        std::vector<line*> cache_lines;
 };
--- include/line.hpp
+++ include/line.hpp
@@ -1,21 +1,19 @@
 #include<cstdint>
 #include<vector>
 
-using namespace std;
-
 class line
 {
     public:
         line(size_t line_size_);
         u_int8_t get_byte(size_t offset_);
-        vector<u_int8_t> get_line_data();
+        std::vector<u_int8_t> get_line_data();
         void write_byte(u_int8_t write_data_, size_t tag_, size_t offset_);
-        void write_line(vector<u_int8_t> write_data_, size_t tag_);
+        void write_line(std::vector<u_int8_t> write_data_, size_t tag_);
         size_t get_tag();
         bool get_valid();
 
     private:
-        vector<u_int8_t> line_data;
+        std::vector<u_int8_t> line_data;
         size_t tag;
         bool valid; 
         size_t line_size;
