95fd5dbf6699cfccb8ec84f8c7253c8f2f2a5fe4
Arjun Tyagi <arjun25@illinois.edu>
2026-07-10 23:38:56 -0500

    Update main.cpp with newer code
    main() function now creates an object of class memory_subsystem and
    sends the trace commands to it for simulation.

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,12 @@
-#include<iostream>
-#include<vector>
-#include<stdlib.h>
-#include<fstream>
-#include<sstream>
-#include<cstdint>
-#include<cstring>
-#include<cmath>
-#include<cache.hpp>
-#include<memory.hpp>
+#include <iostream>
+#include <vector>
+#include <stdlib.h>
+#include <fstream>
+#include <sstream>
+#include <cstdint>
+#include <cstring>
+#include <cmath>
+#include "../include/memory_subsystem.hpp"
 
 using namespace std;
 
@@ -42,9 +41,9 @@ vector<vector<string>> read_trace_file(string trace_file_path)
 
 int main(int argc, char** argv)
 {
-    if (argc < 17)
+    if (argc < 21)
     {
-        cerr << "Missing arguments. Usage: " << argv[0] << " --memory_size <memory size in kB> --page_size <page size in kB> --cache_size <cache size in kB> --line_size <line size in kB> --assoc <associativity> --replacement_policy <specify the replacement policy for the cache> --trace_file <path to trace file> --verbose true" << endl;
+        cerr << "Missing arguments. Usage: " << argv[0] << " --memory_size <memory size in kB> --page_size <page size in kB> --cache_size <cache size in kB> --line_size <line size in kB> --assoc <associativity> --replacement_policy <specify the replacement policy for the cache> --trace_file <path to trace file> --write_policy <write_back/write_through> --write_allocate<true/false> --verbose true" << endl;
         return -1;
     }
     size_t memory_size = 8192;
@@ -53,10 +52,12 @@ int main(int argc, char** argv)
     size_t line_size = 8;
     size_t assoc = 4;
     string replacement_policy = "first_line";
-    string trace_file_path;
+    string write_policy = "write_back";
+    bool write_allocate = false;
+    string trace_file_path = "../test/trace.txt";
     bool verbose = false;
 
-    for (int i = 1; i < 16; i += 2)
+    for (int i = 1; i < 20; i += 2)
     {
         if (!strcmp(argv[i], "--memory_size"))
             memory_size = strtoull(argv[i+1], NULL, 10);
@@ -74,6 +75,10 @@ int main(int argc, char** argv)
             trace_file_path = argv[i+1];
         else if (!strcmp(argv[i], "--verbose"))
             verbose = (!strcmp(argv[i+1], "true"));
+        else if (!strcmp(argv[i], "--write_policy"))
+            write_policy= argv[i+1];
+        else if (!strcmp(argv[i], "--write_allocate"))
+            write_allocate = (!strcmp(argv[i+1], "true")); 
         else
         {
             cerr << "Undefined flag. Usage: " << argv[0] << " --memory_size <memory size in kB> --page_size <page size in kB> --cache_size <cache size in kB> --line_size <line size in kB> --assoc <associativity> --replacement_policy <specify the replacement policy for the cache> --trace_file <path to trace file>" << endl;
@@ -95,7 +100,8 @@ int main(int argc, char** argv)
     cout << "Line size (kB): " << line_size << endl;
     cout << "Associativity: " << assoc << endl;
     cout << "Replacement Policy: " << replacement_policy << endl;
-    cout << "Trace path: " << trace_file_path << endl;
+    cout << "Trace Path: " << trace_file_path << endl;
+    cout << "Verbose Mode: " << verbose << endl;
     cout << "Number of page offset bits: " << page_offset_bits << endl;
     cout << "Number of line offset bits: " << line_offset_bits << endl;
     cout << "Number of index bits: " << index_bits << endl;
@@ -112,13 +118,7 @@ int main(int argc, char** argv)
 
     vector<vector<string>> trace = read_trace_file(trace_file_path);
 
-    memory* M = new memory(memory_size, page_size, line_size, "random");
-    cache* C = new cache(cache_size, line_size, assoc, replacement_policy); 
-
-    cout << "--------------------Initial State of Memory--------------------" << endl;
-    M->print_memory_data();
-    cout << "--------------------Initial State of Cache--------------------" << endl;
-    C->print_cache_data();
+    memory_subsystem* mem_subsys = new memory_subsystem(memory_size, page_size, cache_size, line_size, assoc, replacement_policy, write_policy, write_allocate, verbose);
     
     for (size_t lines = 0; lines < trace.size(); lines++)
     {
@@ -130,86 +130,15 @@ int main(int argc, char** argv)
 
         if (operation == "R")
         {
-            string status;
-            bool evicted = false;
-            optional<u_int8_t> result = C->find_byte(address);
-            if (result.has_value())
-            {
-                status = "HIT";
-            }
-            else
-            {
-                line* l = M->get_line(address);
-                pair<line*, size_t> evicted_line = C->place_line(l, address);
-                if (evicted_line.first != nullptr)
-                {
-                    M->write_line(evicted_line.first, evicted_line.second);
-                    evicted = true;
-                }
-                status = "MISS";
-            }
-
-            if (verbose)
-            {
-                cout << "-----------------------------------------------------------" << endl; 
-                cout << "\tTrace: " << operation << " " << address << endl;
-                cout << "\tTag: " << hex << (address >> (index_bits + line_offset_bits)) << endl;
-                cout << "\tIndex: " << hex << ((address & index_mask) >> line_offset_bits) << endl;
-                cout << "\tLine Offset: " << hex << (address & line_offset_mask) << endl;
-                cout << "\tPage Number: " << hex << (address >> page_offset_bits) << endl;
-                cout << "\tPage Offset: " << hex << (address & page_offset_mask) << endl;
-                cout << "\tStatus: " << status << endl; 
-                cout << "\tEvicted: " << evicted << endl;
-                if (status == "HIT")
-                    cout << "\tRead Value: " << static_cast<size_t>(*result) << endl; 
-                C->print_cache_data();
-                cout << "-----------------------------------------------------------" << endl;
-            }
+            mem_subsys->read(address);
         }
         else if (operation == "W")
         {
-            string status;
-            bool evicted = false;
-            bool write_successful = C->write_byte(address, data);
-            if (write_successful)
-            {
-                status = "HIT";
-            }
-            else
-            {
-                line* requested_line = M->get_line(address);
-                requested_line->write_byte(data, (address >> (index_bits + line_offset_bits)), ((address & index_mask) >> line_offset_bits));
-                pair<line*, size_t> evicted_line = C->place_line(requested_line, address);
-                if (evicted_line.first != nullptr)
-                {
-                    M->write_line(evicted_line.first, evicted_line.second);
-                    evicted = true;
-                }
-                status = "MISS";
-            }
-
-            if (verbose)
-            {
-                cout << "-----------------------------------------------------------" << endl; 
-                cout << "\tTrace: " << operation << " " << address << " " << data << endl;
-                cout << "\tTag: " << hex << (address >> (index_bits + line_offset_bits)) << endl;
-                cout << "\tIndex: " << hex << ((address & index_mask) >> line_offset_bits) << endl;
-                cout << "\tLine Offset: " << hex << (address & line_offset_mask) << endl;
-                cout << "\tPage Number: " << hex << (address >> page_offset_bits) << endl;
-                cout << "\tPage Offset: " << hex << (address & page_offset_mask) << endl;
-                cout << "\tStatus: " << status << endl; 
-                cout << "\tEvicted: " << evicted << endl;
-                C->print_cache_data();
-                cout << "-----------------------------------------------------------" << endl;
-            }
+            mem_subsys->write(address, data);
         }
     }
 
-    cout << "------------------------FINAL STATS------------------------" << endl;
-    cout << "Total Accesses: " << dec << C->total_accesses << endl;
-    cout << "Total Hits: " << dec << C->total_hits << endl;
-    cout << "Total Misses: " << dec << C->total_misses << endl;
-    cout << "-----------------------------------------------------------" << endl;
+    mem_subsys->report_stats();
     
     return 1;
 }
