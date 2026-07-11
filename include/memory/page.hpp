6a7ddc31c5ff05b83d5ba6b37738f775c8f88160
Arjun Tyagi <arjun25@illinois.edu>
2026-07-11 10:38:16 -0500

    Add new function along with other changes
    This commit makes the following changes to page.hpp:
    * Fix #include header for better readability
    * Use memory_line in place of cache_line
    * Add a new function write_byte() that writes a byte of data to the line
      in the page
    * write_line() function takes the line's data as input instead of a
      pointer to the line
    * Added line_offset_bits, line_offset_mask, line_index_bits, and
      line_index_mask

--- include/memory/page.hpp
+++ include/memory/page.hpp
@@ -1,8 +1,8 @@
 #pragma once
 
-#include<vector>
-#include<string>
-#include<line.hpp>
+#include <vector>
+#include <string>
+#include "memory_line.hpp"
 
 class page
 {
@@ -20,14 +20,21 @@ class page
         * @param address_ Address of the byte in the line that needs to be read
         * @return Return a pointer of type `line*` based on the given address
         */
-        line* get_line(size_t address_);
+        memory_line* get_line(size_t address_);
+
+        /*
+        * @brief Write a byte to a line in the page
+        * @param write_data_ `u_int8_t` Data to be written
+        * @param address_ Address of the location where data is to be written
+        */
+        void write_byte(u_int8_t write_data_, size_t address_);
 
         /*
         * @brief Write a line in the page
-        * @param l_ `line*` pointer to the line
+        * @param line_data_ Data of the entire line that is to be written
         * @param address_ Address to any byte within the line from the line offset will be calculated
         */
-        void write_line(line* l_, size_t address_);
+        void write_line(std::vector<u_int8_t> line_data_, size_t address_);
 
         /*
         * @brief Prints the page's data line-by-line
@@ -35,11 +42,16 @@ class page
         void print_page_data();
 
     private:
-        std::vector<line*> page_lines;
+        std::vector<memory_line*> page_lines;
         size_t page_size;
         size_t line_size;
         size_t number_of_lines;
         size_t page_offset_bits;
         size_t page_offset_mask;
-        size_t page_number_mask;
+        
+        size_t line_offset_bits;
+        size_t line_offset_mask;
+
+        size_t line_index_bits;
+        size_t line_index_mask;
 };
