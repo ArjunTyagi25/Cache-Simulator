63a495142d6172124672d17248c1578bd8ff5b13
Arjun Tyagi <arjun25@illinois.edu>
2026-07-12 19:56:21 -0500

    Update script to handle multi-level caches

--- run_simulator.sh
+++ run_simulator.sh
@@ -1,16 +1,38 @@
 config=$1
 
-main_memory_size=$(yq -er '.config.memory_size' "$config")
-page_size=$(yq -er '.config.page_size' "$config")
-cache_size=$(yq -er '.config.cache_size' "$config")
-line_size=$(yq -er '.config.line_size' "$config")
-assoc=$(yq -er '.config.assoc' "$config")
-replacement_policy=$(yq -er '.config.replacement_policy' "$config")
-write_policy=$(yq -er '.config.write_policy' "$config")
-write_allocate=$(yq -er '.config.write_allocate' "$config")
-trace_file=$(yq -er '.config.trace_file' "$config")
-verbose=$(yq -r '.config.verbose' "$config")
+memory_size=$(yq -r '.memory.memory_size' "$config")
+page_size=$(yq -r '.memory.page_size' "$config")
+trace_file=$(yq -r '.simulation.trace_file' "$config")
+verbose=$(yq -r '.simulation.verbose' "$config")
+
+num_levels=$(yq -r '.cache_hierarchy.levels | length' "$config")
+
+cmd=(
+  ./Cache-Simulator
+  --memory_size "$memory_size"
+  --page_size "$page_size"
+  --num_cache_levels "$num_levels"
+  --trace_file "$trace_file"
+  --verbose "$verbose"
+)
+
+for ((i = 0; i < num_levels; i++)); 
+do
+  name=$(yq -r ".cache_hierarchy.levels[$i].name" "$config")
+  cache_size=$(yq -r ".cache_hierarchy.levels[$i].cache_size" "$config")
+  line_size=$(yq -r ".cache_hierarchy.levels[$i].line_size" "$config")
+  assoc=$(yq -r ".cache_hierarchy.levels[$i].assoc" "$config")
+  replacement_policy=$(yq -r ".cache_hierarchy.levels[$i].replacement_policy" "$config")
+  write_policy=$(yq -r ".cache_hierarchy.levels[$i].write_policy" "$config")
+  write_allocate=$(yq -r ".cache_hierarchy.levels[$i].write_allocate" "$config")
+
+  level_config="${name},${cache_size},${line_size},${assoc},${replacement_policy},${write_policy},${write_allocate}"
+
+  cmd+=(--cache_level "$level_config")
+done
 
 cd ./build/
 
-./Cache-Simulator --memory_size $main_memory_size --page_size $page_size --cache_size $cache_size --line_size $line_size --assoc $assoc --replacement_policy $replacement_policy --write_policy $write_policy --write_allocate $write_allocate --verbose $verbose --trace_file $trace_file
+echo "Running: ${cmd[@]}"
+
+"${cmd[@]}"
