b6c73c773fff4a448fbcaac22c27ab2891d36baf
Arjun Tyagi <arjun25@illinois.edu>
2026-07-12 22:20:26 -0500

    Update bash to support multi-level memories

--- run_simulator.sh
+++ run_simulator.sh
@@ -1,22 +1,32 @@
 config=$1
 
-memory_size=$(yq -r '.memory.memory_size' "$config")
-page_size=$(yq -r '.memory.page_size' "$config")
 trace_file=$(yq -r '.simulation.trace_file' "$config")
 verbose=$(yq -r '.simulation.verbose' "$config")
 
-num_levels=$(yq -r '.cache_hierarchy.levels | length' "$config")
+num_memory_levels=$(yq -r '.memory_hierarchy.levels | length' "$config")
+num_cache_levels=$(yq -r '.cache_hierarchy.levels | length' "$config")
 
 cmd=(
   ./Cache-Simulator
-  --memory_size "$memory_size"
-  --page_size "$page_size"
-  --num_cache_levels "$num_levels"
+  --num_memory_levels "$num_memory_levels"
+  --num_cache_levels "$num_cache_levels"
   --trace_file "$trace_file"
   --verbose "$verbose"
 )
 
-for ((i = 0; i < num_levels; i++)); 
+for ((i = 0; i < num_memory_levels; i++)); 
+do
+  name=$(yq -r ".memory_hierarchy.levels[$i].name" "$config")
+  memory_size=$(yq -r ".memory_hierarchy.levels[$i].memory_size" "$config")
+  page_size=$(yq -r ".memory_hierarchy.levels[$i].page_size" "$config")
+  line_size=$(yq -r ".memory_hierarchy.levels[$i].line_size" "$config")
+
+  level_config="${name},${memory_size},${page_size},${line_size}"
+
+  cmd+=(--memory_level "$level_config")
+done
+
+for ((i = 0; i < num_cache_levels; i++)); 
 do
   name=$(yq -r ".cache_hierarchy.levels[$i].name" "$config")
   cache_size=$(yq -r ".cache_hierarchy.levels[$i].cache_size" "$config")
