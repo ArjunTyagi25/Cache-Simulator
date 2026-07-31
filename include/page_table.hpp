80ac66b9f60e7a8037f756e88b457e7a5a076a2b
ArjunTyagi25 <arjun25@illinois.edu>
2026-07-30 19:10:25 -0500

    Add code for page_table class
    This commit adds the initial code for the page_table class, used to
    define a page table of any level i.e., it could either contain pointer
    to lower-level page tables or pointers to page_table_entries.

---
+++ include/page_table.hpp
@@ -0,0 +1,45 @@
+#pragma once
+
+#include <vector>
+#include <variant>
+#include <optional>
+
+#include "page_table_entry.hpp"
+
+class page_table
+{
+    private:
+        /// @brief Number of entries in the page table
+        size_t num_entries;
+        /// @brief Vector containing the content of the page table 
+        std::vector<std::variant<std::monostate, page_table*, page_table_entry*>> page_table_;
+
+    public:
+        /**
+         * @brief Construct a new page table object
+         * 
+         * @param page_size_ Size of the page
+         * @param page_table_entry_size_ Size of a page table entry
+         * @param last_level_page_table_ `true` if this is the lowest level page table in a multi-level page table; `false` otherwise
+         */
+        page_table(size_t page_size_,
+                   size_t page_table_entry_size_,
+                   bool last_level_page_table_);
+
+        /**
+         * @brief Find and fetch an entry from the page table
+         * 
+         * @param index_ Index number in the page table that you want to fetch
+         * @return `std::variant<std::monostate, page_table*, page_table_entry*>` i.e., either monostate representing empty slot, pointer to a `page_table`, or a pointer to a `page_table_entry`
+         */
+        std::variant<std::monostate, page_table*, page_table_entry*> find_entry(size_t index_);
+
+        /**
+         * @brief Insert a new entry into the page table
+         * 
+         * @param index_ Index number in the page table where you want to insert the entry
+         * @param entry_ Pointer to the entry, which could either be `page_table*` or `page_table_entry*`
+         */
+        void insert_entry(size_t index_, 
+                          std::variant<page_table_entry*, page_table*> entry_);
+};
---
+++ src/page_table.cpp
@@ -0,0 +1,60 @@
+#include <cstdio>
+#include <vector>
+
+#include "../include/page_table.hpp"
+
+using namespace std;
+
+page_table::page_table(size_t page_size_,
+                       size_t page_table_entry_size_,
+                       bool last_level_page_table_)
+{
+    this->num_entries = page_size_/page_table_entry_size_;
+
+    for (size_t i = 0; i < this->num_entries; i++)
+    {
+        if (last_level_page_table_)
+        {
+            page_table_entry* PTE = new page_table_entry(0, false, false);
+            this->page_table_.push_back(PTE);
+        }
+        else
+        {
+            this->page_table_.push_back(monostate{});
+        }
+    }
+}
+
+variant<monostate, page_table*, page_table_entry*> page_table::find_entry(size_t index_)
+{
+    // Entry in the page table is empty/NULL
+    if (std::holds_alternative<std::monostate>(this->page_table_[index_]))
+    {
+        return std::monostate{};
+    }
+    // Entry in the page table is a pointer to lower-level page table
+    else if (std::holds_alternative<page_table*>(this->page_table_[index_]))
+    {
+        return std::get<page_table*>(this->page_table_[index_]);
+    }
+    // Entry in the page table is a PTE pointing to a physical page number
+    else if (std::holds_alternative<page_table_entry*>(this->page_table_[index_]))
+    {
+        return std::get<page_table_entry*>(this->page_table_[index_]);
+    }
+    else
+        return std::monostate{};
+}
+
+void page_table::insert_entry(size_t index_,
+                              variant<page_table_entry*, page_table*> entry_)
+{
+    if (std::holds_alternative<page_table_entry*>(entry_))
+    {
+        this->page_table_[index_] = std::get<page_table_entry*>(entry_);
+    }
+    else if (std::holds_alternative<page_table*>(entry_))
+    {
+        this->page_table_[index_] = std::get<page_table*>(entry_);
+    }
+}
