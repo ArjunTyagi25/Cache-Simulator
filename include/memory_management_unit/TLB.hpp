5309590d148eab36022115d3b7156842ca5e3487
ArjunTyagi25 <arjun25@illinois.edu>
2026-08-02 14:49:15 -0500

    Update update_entry() and find_entry() functions
    This commits updates the TLB code in accordance with the new
    page_table_entry wherein the VPN of an entry is stored in the
    page_table_entry itself. Also added a function to print the contents of
    the TLB.

--- include/memory_management_unit/TLB.hpp
+++ include/memory_management_unit/TLB.hpp
@@ -10,36 +10,42 @@
 class TLB
 {
     private:
-    /// @brief Number of page table entries in the TLB
-    size_t num_entries;
-    /// @brief Vector holding all the page table entries residing in the TLB
-    std::map<size_t, page_table_entry*> TLB_content;
-    /// @brief Pseudo-random number generator used for evicting entries from the TLB
-    std::mt19937 gen;
+        /// @brief Number of page table entries in the TLB
+        size_t num_entries;
+        /// @brief Vector holding all the page table entries residing in the TLB
+        std::vector<page_table_entry*> TLB_content;
+        /// @brief Pseudo-random number generator used for evicting entries from the TLB
+        std::mt19937 gen;
 
     public:
-    /**
-     * @brief Construct a new TLB object
-     * 
-     * @param num_entries_ Size of the TLB in terms of the number of page table entries it is hold
-     */
-    TLB(size_t num_entries_);
+        /**
+         * @brief Construct a new TLB object
+         * 
+         * @param num_entries_ Size of the TLB in terms of the number of page table entries it is hold
+         */
+        TLB(size_t num_entries_);
 
-    /**
-     * @brief Find an entry in the TLB
-     * 
-     * @param VPN_ VPN from the virtual address
-     * @return `page_table_entry*` representing the `page_table_entry` associated with the given `VPN_` 
-     */
-    page_table_entry* find_entry(size_t VPN_);
+        /**
+         * @brief Find an entry in the TLB
+         * 
+         * @param VPN_ VPN from the virtual address
+         * @return `page_table_entry*` representing the `page_table_entry` associated with the given `VPN_` 
+         */
+        page_table_entry* find_entry(size_t VPN_);
 
-    /**
-     * @brief Insert an entry into the TLB
-     * 
-     * @param VPN_ VPN from the virtual address
-     * @param entry_ `page_table_entry*` that you want to insert
-     * @return `page_table_entry*` representing the evicted `page_table_entry` from the TLB, if any. 
-     */
-    page_table_entry* insert_entry(size_t VPN_,
-                                   page_table_entry* entry_);  
+        /**
+         * @brief Update an entry into the TLB
+         * 
+         * @param VPN_ VPN from the virtual address associated with the entry
+         * @param PFN_ PFN from the physical address associated with the entry
+         * @param dirty_bit_ Dirty bit associated with the entry
+         * @param valid_bit_ Valid bit associated with the entry
+         * @return `page_table_entry*` representing the evicted `page_table_entry` from the TLB, if any. 
+         */
+        page_table_entry* update_entry(size_t VPN_,
+                                       size_t PFN_,
+                                       bool dirty_bit_,
+                                       bool valid_bit_);
+
+        void print_TLB_data();
 };
--- src/memory_management_unit/TLB.cpp
+++ src/memory_management_unit/TLB.cpp
@@ -7,43 +7,69 @@ using namespace std;
 TLB::TLB(size_t num_entries_)
 {
     this->num_entries = num_entries_;
+
+    for (size_t i = 0; i < this->num_entries; i++)
+    {
+        page_table_entry* PTE = new page_table_entry(0, 0, false, false);
+        this->TLB_content.push_back(PTE);
+    }
     this->gen = mt19937(0);
 }
 
 page_table_entry* TLB::find_entry(size_t VPN_)
 {
-    if (this->TLB_content.find(VPN_) != this->TLB_content.end())
-        return this->TLB_content[VPN_];
-    else
-        return nullptr;
+    for (size_t i = 0; i < this->num_entries; i++)
+    {
+        if ((this->TLB_content[i]->get_VPN() == VPN_) & this->TLB_content[i]->get_valid_bit())
+            return this->TLB_content[i];
+    }
+    return nullptr;
 }
 
-page_table_entry* TLB::insert_entry(size_t VPN_,
-                                    page_table_entry* entry_)
+page_table_entry* TLB::update_entry(size_t VPN_,
+                                    size_t PFN_,
+                                    bool dirty_bit_,
+                                    bool valid_bit_)
 {
-    // Check if the VPN_ already existed in the TLB
-    if (this->TLB_content.find(VPN_) != this->TLB_content.end())
+    // If the VPN_ already existed in the TLB
+    for (size_t i = 0; i < this->num_entries; i++)
     {
-        this->TLB_content[VPN_] = entry_;
-        return nullptr;
+        if ((this->TLB_content[i]->get_VPN() == VPN_) & this->TLB_content[i]->get_valid_bit())
+        {
+            this->TLB_content[i]->update_entry(VPN_, PFN_, dirty_bit_, valid_bit_);
+            return nullptr;
+        }
     }
-    // Since the VPN_ does not exist in the TLB, we have to insert it. We first check if the current number of entries in the TLB is less than the size of TLB. If so, we just insert the new entry and return nullptr to indict nothing was evicted
-    else if (this->TLB_content.size() < this->num_entries)
+
+    // VPN_ does not exist in the TLB. First, we check if there is an entry with valid bit as false
+    for (size_t i = 0; i < this->num_entries; i++)
     {
-        this->TLB_content[VPN_] = entry_;
-        return nullptr;
+        if (!this->TLB_content[i]->get_valid_bit())
+        {
+            this->TLB_content[i]->update_entry(VPN_, PFN_, dirty_bit_, valid_bit_);
+            return nullptr;
+        }
     }
-    else if (this->TLB_content.size() == this->num_entries)
+
+    // VPN_ does not exist in the TLB and there is no empty space, so need to evict an entry from the TLB
+    uniform_int_distribution<size_t> dist(0, this->num_entries);
+    size_t entry_to_evict = dist(this->gen);
+    
+    size_t evicted_entry_VPN = this->TLB_content[entry_to_evict]->get_VPN();
+    size_t evicted_entry_PFN = this->TLB_content[entry_to_evict]->get_PFN();
+    bool evicted_entry_dirty_bit = this->TLB_content[entry_to_evict]->get_dirty_bit();
+    bool evicted_entry_valid_bit = this->TLB_content[entry_to_evict]->get_valid_bit();
+
+    this->TLB_content[entry_to_evict]->update_entry(VPN_, PFN_, dirty_bit_, valid_bit_);
+    page_table_entry* evicted_entry = new page_table_entry(evicted_entry_VPN, evicted_entry_PFN, evicted_entry_dirty_bit, evicted_entry_valid_bit);
+    return evicted_entry;
+}
+
+void TLB::print_TLB_data()
+{
+    cout << "Entry #\tDirty\tValid\tVPN\tPFN" << endl;
+    for (size_t i = 0; i < this->num_entries; i++)
     {
-        uniform_int_distribution<size_t> dist(0, this->num_entries);
-        size_t entry_to_evict = dist(this->gen);
-        page_table_entry* evicted_entry = this->TLB_content[entry_to_evict];
-        this->TLB_content[VPN_] = entry_;
-        return evicted_entry;
-    }
-    else
-    {
-        cout << "Number of entries in the TLB exceed the maximum size. Please check your code implementation!" << endl;
-        return nullptr;
+        cout << i << "\t" << this->TLB_content[i]->get_dirty_bit() << "\t" << this->TLB_content[i]->get_valid_bit() << "\t" << hex << this->TLB_content[i]->get_VPN() << "\t" << this->TLB_content[i]->get_PFN() << endl; 
     }
 }
