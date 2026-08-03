cdd013f7bb07fabb9bd16b26f27428bcc19a7471
ArjunTyagi25 <arjun25@illinois.edu>
2026-08-02 19:15:21 -0500

    Add Doxygen-style comments

--- include/memory/memory.hpp
+++ include/memory/memory.hpp
@@ -7,8 +7,23 @@
 
 class memory
 {
+    private:
+        /// @brief Size of memory in bytes (B)
+        size_t memory_size;
+        /// @brief Size of a page in bytes (B)
+        size_t page_size;
+        /// @brief Size of a memory line in bytes (B)
+        size_t line_size;
+        /// @brief Number of pages in the memory
+        size_t number_of_pages;
+        /// @brief Number of bits in the address for page offset
+        size_t page_offset_bits;
+        /// @brief Vector holding all the pages in the memory
+        std::vector<page*> pages;
+        /// @brief Generator
+        std::mt19937 gen;
     public:
-        /*
+        /**
         * @brief Constructor for the memory class
         * @param memory_size_ Size of the memory
         * @param page_size_ Size of the page within the memory
@@ -17,21 +32,21 @@ class memory
         */
         memory(size_t memory_size_, size_t page_size_, size_t line_size, std::string init_);
 
-        /*
+        /**
         * @brief Read a line from the memory
         * @param address_ Address of the byte within the line that needs to be read
         * @return Pointer to the line of type `line*`
         */
         memory_line* get_line(size_t address_);
 
-        /*
+        /**
         * @brief Write a byte to memory
         * @param write_data_ Data to be written
         * @param address_ Address of the location where data is to be written
         */
         void write_byte(u_int8_t write_data_, size_t address_);
 
-        /*
+        /**
         * @brief Update a line in the memory
         * @param write_data Data of the entire line that is to be written to the memory
         * @param address_ Address to any byte within the line from which the page number will be calculated
@@ -46,18 +61,8 @@ class memory
          */
         size_t get_free_physical_frame(std::string allocation_policy_);
 
-        /*
+        /**
         * @brief Prints all the memory's content
         */
         void print_memory_data();
-
-    private:
-        size_t memory_size;
-        size_t page_size;
-        size_t line_size;
-        size_t number_of_pages;
-        size_t page_offset_bits;
-        std::vector<page*> pages;
-
-        std::mt19937 gen;
 };
